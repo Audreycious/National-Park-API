90a1913018380f20529608ad3908a0416ff5d4b3
Audrey Foss <porcupine4@gmail.com>
2019-04-20 14:10:25 -0500

    end of Saturday with Prashant

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
-function AddStateDropdown()
+
+
+
+
+
+
+
+
+$('#state-selection-form').submit(function(){
+let ddSelect = [];
+    ddSelect = $('.state-select').val();
+    let maxCnt = $('#frm-MaxCount').val();
+    CallAPi(maxCnt,ddSelect);
+});
+
+// a function to call the API with the query parameters
+    // promise
+
+function CallAPi(maxCount,selectedStates)
 {
-    //Adds the state dropdown dynamically on the form
 
 }
-$('#btnAddState').on('click',function(){
-    AddStateDropdown();
-});
+
+
+
+
+
+
+// a function to validate or ignore duplicate state entries
+
+// a function to create an array of the state entries
+
+// a function to create the headers we send to the API
+
+// a function (or some attributes to the form) to block the     max results entry from submitting the form 
+
+// a function that maps each result 'key' from the responseJson into an array
